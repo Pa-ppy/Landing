94f525b102c74c49121a7095f78fd755056eb046
Pa-ppy <92393021+Pa-ppy@users.noreply.github.com>
2021-10-13 15:30:48 +0000

    Add files via upload

---
+++ landing.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Bestphones</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<div>
+		<div id="header">
+			<ul style="display: inline;">
+				<li><a class="active" href="landing.html"> Home</a></li>
+				<li><a href="#news"> About</a></li>
+				<li><a href="#contact"> News</a></li>
+				<li><a href="#about"> Contact</a></li>
+			</ul>		
+		</div>
+
+		</table>
+		<div id="container">
+			<p>
+				Your one stop phone services.
+			</p>
+				<div class="innertext">
+					<table>
+						<tr>
+							<td class="option"><a href=""> Buy</a></td>
+						</tr>
+						<tr>
+							<td class="option"><a href=""> Sell</a></td>
+						</tr>
+						<tr>
+							<td class="option"><a href=""> Swap</a></td>
+						</tr>
+					</table>
+				</div>
+		</div>
+	</div>
+</body>
+</html>
